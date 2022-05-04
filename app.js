21d2fd41c43d8e92cbce2b1912bc3d299d3704b0
M.Chernisheva <m.chernisheva@dunice.net>
2022-05-04 17:36:30 +0300

    Routs, controllers, association by authorization

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const newsRouter = require('./routes/newsRoutes');
+const authRouter = require('./routes/authRouter');
 
 const app = express();
+
+const NOT_FOUND = 404;
+const INTERNAL_SERVER_ERROR = 500;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -24,16 +28,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
-  next(createError(404));
+  next(createError(NOT_FOUND));
 });
 
 app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
+  res.status(err.status || INTERNAL_SERVER_ERROR);
   res.render('error');
 });
 
---
+++ controllers/authControllers.js
@@ -0,0 +1,69 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { User, News } = require('../models');
+
+const salt = bcrypt.genSaltSync(10);
+
+const OK = 200;
+const BAD_REQUEST = 400;
+
+module.exports = {
+  signup(req, res) {
+    const { login, password, email } = req.body;
+
+    if (!login || !password || !email) {
+      return res.status(BAD_REQUEST).send({ message: 'Enter data, please' });
+    }
+
+    return User.findOrCreate({
+      where: {
+        email,
+      },
+      defaults: {
+        login,
+        password: bcrypt.hashSync(password, salt),
+        email,
+      },
+    })
+      .then((user) => {
+        if (!user[1]) return res.status(BAD_REQUEST).send({ message: 'User is exist' });
+        return res.status(OK).send(user);
+      })
+      .catch((error) => res.status(BAD_REQUEST).send(error));
+  },
+
+  login(req, res) {
+    const { password, email } = req.body;
+
+    if (!password || !email) {
+      return res.status(BAD_REQUEST).send({ message: 'Enter password or email, please' });
+    }
+
+    return User.findOne({
+      where: {
+        email,
+      },
+      include: [{
+        model: News,
+        as: 'news',
+        required: false,
+      }],
+    })
+      .then((user) => {
+        if (!user) {
+          return res.status(BAD_REQUEST).send({ message: 'User is not found' });
+        }
+        const passwordsValid = bcrypt.compareSync(password, user.password);
+        if (!passwordsValid) return res.status(BAD_REQUEST).send({ massage: 'Invalid password!' });
+
+        const token = jwt.sign(user.id, process.env.SECRET_KEY);
+
+        return res.status(OK).json({
+          user,
+          token: `Bearer ${token}`,
+        });
+      })
+      .catch((error) => res.status(BAD_REQUEST).send(error));
+  },
+};
