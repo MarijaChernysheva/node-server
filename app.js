82c5ce051e57a761864c28d59aff6603b988adca
M.Chernisheva <m.chernisheva@dunice.net>
2022-05-30 10:20:14 +0300

    Editing usersRoute

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// const bodyParser = require('body-parser');
 
 const usersRouter = require('./routes/usersRoute');
 const newsRouter = require('./routes/newsRoute');
@@ -19,8 +20,9 @@ app.set('view engine', 'pug');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -32,4 +32,30 @@ module.exports = {
       .then((author) => res.status(OK).send(author))
       .catch((error) => res.status(BAD_REQUEST).send(error));
   },
+  editUser(req, res) {
+    const { login } = req.body;
+    const { file } = req;
+
+    return User.findByPk(req.userId, {
+      include: [{
+        model: News,
+        as: 'news',
+        required: false,
+      }],
+    })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({
+            message: 'User Not Found',
+          });
+        }
+        return user
+          .update({ login, avatar: file?.path })
+          .then((editUser) => {
+            res.status(200).send(editUser);
+          })
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
 };
