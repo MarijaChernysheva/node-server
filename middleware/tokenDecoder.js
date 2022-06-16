9b49ca2cebf65cf46ffa025c2bb245c2991812be
Marija <marija@Mac-mini-Dunice-7.local>
2022-06-16 15:08:58 +0300

    Deleting comments and console.log(back)

--- middleware/tokenDecoder.js
+++ middleware/tokenDecoder.js
@@ -3,11 +3,9 @@ require('dotenv').config();
 
 const FORBIDDEN = 403;
 
-
 const tokenDecoder = (req, res, next) => {
   const token = req.headers.authorization;
   const userId = jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
-    console.log(err)
     if (err) return res.status(FORBIDDEN).send();
     return data;
   });
